3a7f44225d5f3a10808a9fff2c950511c28d4edd
areeshadev <94612095+areeshadev@users.noreply.github.com>
2021-11-18 12:34:05 +0100

    Create script.js

---
+++ Javascript/Week 01/script.js
@@ -0,0 +1,104 @@
+// one 
+
+console.log ('hello world!')
+
+// two
+
+console.log('I\'m awesome');
+
+// three
+
+const x= 5
+console.log ('the value of my variable will be : 5')
+console.log (x)
+
+const x= 10
+console.log ('the value of my variable will be: 10')   // I got an error 
+console.log (x)
+
+// Four
+
+const y = 'one'
+console.log ('the value of the string is a one')
+console.log (y)
+
+const y = 'two'
+console.log ('will be display and error since a const cannot be changed')
+
+// Five
+
+const z = 7.25
+console.log (z)
+
+const a = 7
+console.log (a)
+
+ a < z
+
+const x = z
+console.log (x)
+
+// Six
+
+
+   
+const elements = []
+console.log ('the array is empty')
+console.log (elements)
+
+const favoriteAnimals = ['cat', 'dog', 'rabbit']
+console.log (favoriteAnimals)
+
+const newFavoriteAnimals = [...favoriteAnimals, 'baby pig']
+
+console.log (newFavoriteAnimals)
+
+// Seven
+
+let myString = "this is a test"
+console.log (myString)
+
+console.log (myString.length)
+
+// Eight
+
+let one = 2
+console.log (one)
+console.log ('the type of my variable is primitive because it is a number, and it is inmutable')
+console.log (typeof one);
+
+let two = 4
+console.log (two)
+
+let three = 6
+console.log (three)
+
+let four = 8
+console.log (four)
+
+two > one && three < four
+
+if (one = two) {
+  console.log ('SAME TYPE');}
+
+  // Nine
+
+  let x = 7
+
+x = x %3     // value of x is 1
+
+
+console.log ('first check how many times the small number can fit inside the big number')
+console.log ('that it means that shared the number in smaller portions')
+console.log ('the result will be the remining of the proportional division of the big number')
+
+
+// Ten
+
+   
+const mixedArray = ['one chair', 'two chairs', 1, 2]
+console.log (mixedArray)
+
+console.log (6/0 === 10/0)
+
+console.log ('with this operator we can compare and ask for strict equality')
